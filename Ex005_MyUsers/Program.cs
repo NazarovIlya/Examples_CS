2686414c9e4eb8c86e1ca0c86829717a11584e03
NazarovIlya <eliakohai@gmail.com>
2022-07-08 10:24:48 +0300

    My users

---
+++ Ex005_MyUsers/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "илья")
+{
+    Console.Write("Приветствую, мой гениальный господин и создатель, Илья!");
+} 
+else if(username.ToLower() == "наталья")
+{
+    Console.Write("Приветствую тебя, любовь моего господина, Наталья!");
+}
+else 
+{ 
+    Console.Write("Привет, " );
+    Console.Write(username);
+    Console.WriteLine("!");
+}
