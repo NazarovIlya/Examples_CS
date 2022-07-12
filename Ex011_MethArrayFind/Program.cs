88dd14dc5771d9de230e2587fc6bee0e5fbcddaf
NazarovIlya <eliakohai@gmail.com>
2022-07-12 17:28:35 +0300

    array method_find

---
+++ Ex011_MethArrayFind/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int[] array = { 100, 265, 300, 14, 54, 664, 75, 80, 90 };
+
+int n = array.Length;
+int find = 664;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        // break;
+    }
+    index++;
+}
