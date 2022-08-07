8b7b4dc9c04de1c4a446767ede7ca42754254590
NazarovIlya <eliakohai@gmail.com>
2022-08-07 20:44:28 +0300

    just a fibonacci number

--- Ex020_Fibonacci/Program.cs
+++ Ex020_Fibonacci/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// f(1) = 1; 
+// f(2) = 1;
+// f(n) = fn-1 + fn-2
+
+int GetUserInputInt(string userInputStr)
+{
+    Console.WriteLine(userInputStr);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+double GetFibonacci(int number)
+{
+    if (number == 1 || number == 2) return 1;
+    else return GetFibonacci(number - 1) + GetFibonacci(number - 2);
+}
+
+Console.Clear();
+int numFi = GetUserInputInt("Введите номер числа Фибоначчи: ");
+double fibo = GetFibonacci(numFi);
+Console.WriteLine($"Число Фибоначчи под номером {numFi} -->{fibo}");
