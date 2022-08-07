783fce621a9d96e51111b09ce74dd1c5fd94d0fe
NazarovIlya <eliakohai@gmail.com>
2022-08-07 13:23:00 +0300

    PrintMatrixInt method is completed

---
+++ Ex017_Matrix/Program.cs
@@ -0,0 +1,27 @@
+﻿// 
+
+
+int GetUserInputInt(string userInputStr)
+{
+    Console.WriteLine(userInputStr);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void PrintMatrixInt(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+int nRows = GetUserInputInt("Введите количество строк: ");
+int nColumns = GetUserInputInt("Введите количество столбцов: ");
+int[,] matrix = new int[nRows, nColumns];
+PrintMatrixInt(matrix);
