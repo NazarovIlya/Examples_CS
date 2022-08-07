8d6c7a39bf79a9c87e2084c0856f265173391808
NazarovIlya <eliakohai@gmail.com>
2022-08-07 20:02:25 +0300

    just a factorial

---
+++ Ex019_Factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿int GetUserInputInt(string userInputStr)
+{
+    Console.WriteLine(userInputStr);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+double GetFactorial(int number)
+{
+    if (number == 0) return 1;
+    else if (number == 1) return 1;
+    else return number * GetFactorial(number - 1);
+}
+
+
+Console.Clear();
+int numF = GetUserInputInt("Факториал какого числа необходимо найти? ");
+double fact = GetFactorial(numF);
+Console.WriteLine($"Факотриал числа {numF} равен {fact}");
